0b9147774d7f5f9d72124f62b666f32f5b75b579
RealThadeous <bor21004@byui.edu>
2021-10-23 20:00:28 +0200

    week 6 preston updates

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,11 @@
-let t = parseFloat(document.querySelector("#temp").textContent);
-let ws = parseFloat(document.querySelector("#windspeed").textContent);
-let wc = '';
-if (t <= 50 && ws > 3){
-    wind_chill = 35.74 + 0.6215 * t - 35.75 * (ws ** 0.16) + 0.4275 * t * (ws ** 0.16);
-    ws = wind_chill.toFixed(2) + " &deg; F";
-    document.querySelector("#windchill").innerHTML = ws;
+let windspeed = parseFloat(document.querySelector("#windspeed").textContent);
+let temp = parseFloat(document.querySelector("#temp").textContent);
+let windchill = '';
+
+if (temp <= 50 && windspeed > 3){
+    wc = 35.74 + 0.6215 * temp - 35.75 * (windspeed ** 0.16) + 0.4275 * temp * (windspeed ** 0.16);
+    windspeed = wc.toFixed(2) + " &deg; F";
+    document.querySelector("#windchill").innerHTML = windspeed;
 }
 else{
     document.querySelector("#windchill").innerHTML = "N/A";
