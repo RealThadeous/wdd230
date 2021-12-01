aa8c8b234d07e5be1a0541adbcb5d3136c62db2d
RealThadeous <bor21004@byui.edu>
2021-12-02 00:43:09 +0200

    update week 11 weather apis

---
+++ lesson11/js/weatherapi-fish.js
@@ -0,0 +1,31 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=bfec02243921903c293dcb0c1b72d77d&units=imperial'
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('currently').textContent = jsObject.list[0].weather[0].main;
+    temp = jsObject.list[0].main.temp;
+    document.getElementById('temp').textContent = temp;
+    document.getElementById('humidity').textContent = jsObject.list[0].main.humidity;
+    document.getElementById('windspeed').textContent = jsObject.list[0].wind.speed;
+    
+    let forecast = jsObject['list'];
+    for (let i of [6, 14, 22, 30, 38]) {
+      // if (forecast[i].dt_txt.includes("18:00:00")){
+        let td = document.createElement('td');
+        let img = document.createElement('img');
+        let high = document.createElement('span');
+        const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';  
+        const desc = forecast[i].weather[0].description;  
+        
+        img.setAttribute('src', imagesrc);
+        img.setAttribute('alt', desc);
+        high.innerHTML = `${forecast[i].main.temp.toFixed(0)}&deg;F`;
+
+        td.appendChild(img);
+        td.appendChild(high);
+
+        document.querySelector('tr.forecast-td').appendChild(td);
+    }
+  });
